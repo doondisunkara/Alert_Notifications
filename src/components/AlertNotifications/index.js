cccc62876f38ff024868a8d08eb96b35ed0b3b61
doondisunkara <sunkaranag2004@gmail.com>
2024-10-23 12:11:51 +0000

    Elements sent as children

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,3 +1,7 @@
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import {MdWarning, MdInfo} from 'react-icons/md'
+
 import Notification from '../Notification'
 
 import './index.css'
@@ -5,36 +9,60 @@ import './index.css'
 const notificationsList = [
   {
     id: 'SUCCESS',
+    icon: 'AiFillCheckCircle',
     heading: 'Success',
     description: 'You can access all the files in the folder',
   },
   {
     id: 'ERROR',
+    icon: 'RiErrorWarningFill',
     heading: 'Error',
     description:
       'Sorry, you are not authorized to have access to delete the file',
   },
   {
     id: 'WARNING',
+    icon: 'MdWarning',
     heading: 'Warning',
     description: 'Viewers of this file can see comments and suggestions',
   },
   {
     id: 'INFO',
+    icon: 'MdInfo',
     heading: 'Info',
     description: 'Anyone on the internet can view these files',
   },
 ]
 
-const AlertNotifications = () => (
-  <div className="alert-notification-container">
-    <h1 className="main-heading">Alert Notifications</h1>
-    <ul className="notifications-list-container">
-      {notificationsList.map(each => (
-        <Notification notificationDetails={each} key={each.id} />
-      ))}
-    </ul>
-  </div>
-)
+const AlertNotifications = () => {
+  const getIcon = id => {
+    switch (id) {
+      case 'SUCCESS':
+        return <AiFillCheckCircle size={50} className="success" />
+      case 'ERROR':
+        return <RiErrorWarningFill size={50} className="error" />
+      case 'WARNING':
+        return <MdWarning size={50} className="warning" />
+      default:
+        return <MdInfo size={50} className="info" />
+    }
+  }
 
+  return (
+    <div className="alert-notification-container">
+      <h1 className="main-heading">Alert Notifications</h1>
+      <ul className="notifications-list-container">
+        {notificationsList.map(each => (
+          <Notification notificationDetails={each} key={each.id}>
+            {{
+              Icon: getIcon(each.id),
+              Heading: each.heading,
+              Description: each.description,
+            }}
+          </Notification>
+        ))}
+      </ul>
+    </div>
+  )
+}
 export default AlertNotifications
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,35 +1,20 @@
-import {AiFillCheckCircle} from 'react-icons/ai'
-import {RiErrorWarningFill} from 'react-icons/ri'
-import {MdWarning, MdInfo} from 'react-icons/md'
 import {GrFormClose} from 'react-icons/gr'
 
 import './index.css'
 
 const Notification = props => {
-  const {notificationDetails} = props
-  const {id, heading, description} = notificationDetails
-
-  const getIcon = () => {
-    switch (id) {
-      case 'SUCCESS':
-        return <AiFillCheckCircle size={50} className="success" />
-      case 'ERROR':
-        return <RiErrorWarningFill size={50} className="error" />
-      case 'WARNING':
-        return <MdWarning size={50} className="warning" />
-      default:
-        return <MdInfo size={50} className="info" />
-    }
-  }
+  const {children} = props
+  const {Icon, Heading, Description} = children
+  // console.log(props)
 
   return (
     <li className="notification-item">
-      {getIcon()}
+      {Icon}
       <div className="details-container">
-        <h1 className={`notification-heading ${heading.toLowerCase()}`}>
-          {heading}
+        <h1 className={`notification-heading ${Heading.toLowerCase()}`}>
+          {Heading}
         </h1>
-        <p className="notification-description">{description}</p>
+        <p className="notification-description">{Description}</p>
       </div>
       <GrFormClose size={50} className="close-icon" />
     </li>
